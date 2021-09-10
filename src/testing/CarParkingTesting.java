78f0eb522798bdd9f9ed97004f4c37fd7632a673
Johannes <johagg17@student.hh.se>
2021-09-10 15:53:01 +0200

    First commit of project

---
+++ src/testing/CarParkingTesting.java
@@ -0,0 +1,17 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CarParkingTesting {
+
+	@Test
+	public void test() {
+		boolean test1 = true;
+		assertTrue(test1);
+	
+		
+	}
+
+}
