5729851859bc4e23d178f97add56417b929b279d
Johannes Hägglund <johagg17@student.hh.se>
2021-09-13 14:43:57 +0200

    Added new functionality

--- src/productioncode/CarParking.java
+++ src/productioncode/CarParking.java
@@ -1,15 +1,110 @@
 package productioncode;
+import java.lang.Math;
 
 public class CarParking {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello");
-		boolean b = true;
-		System.out.println(b == true);
+	int x = 0;
+	int y = 0;
+	/**
+	 * Method that will return the current position and the situation of the car, (x, y).  
+	 * @param none
+	 * @precondition 0 <= y <= 500, 0 <= x <= 1
+	 * @postcondition 0 <= y <= 500, 0 <= x <= 1
+	 * @return int [] of current position
+	 * 
+	 */
+	public int [] WhereIs() {
+		// TODO Auto-generated method stub
+		int[] position = {x,y};
+		return position;	
+	}
+	/**
+	 * Method that will query the two sensors at least 5 times, 
+	 * and return the distance to the nearest parking-spot
+	 * @param None
+	 * @precondition 0 <= y <= 500, x=0
+	 * @postcondition 0 <= y <= 500, x=0,  0 <= sensor values <= 200
+	 * @return int y, distance to nearest parking-spot. 
+	 */
+	public int isEmpty() {
+		// TODO Auto-generated method stub
+		int sensor1 = 0;
+		int sensor2 = 0;
+		int n = 5;
+		for (int i=0; i < n; i++ ) {
+			sensor1 += (int) (Math.random() * 200);
+			sensor2 += (int) (Math.random() * 200);
+		}
+		// To check if a sensor is very noise, we could use standard deviation
+		sensor1 = sensor1 / n;
+		sensor2 = sensor2 / n;
+		return Math.min(sensor1, sensor2);
+	}
+	/**
+	 * 
+	 * @param None
+	 * @return 
+	 * @precondition y < 500, x = 0
+	 * @postcondition y = previous_pos + 1, x = 0, y <= 500
+	 * @return a data structure containing the new position and the situation (parked / unparked)
+	 */
+	public int[] MoveForward() {
+		// TODO Auto-generated method stub
+		this.y += 1;
+		int distance = this.isEmpty();
+		int [] currentSituation = {x, y, distance};
+		
+		return currentSituation;
+		
 	}
 	
-
+	/**
+	 * The method will move the car forward 1 meter and move it out from the parking lot to the street.
+	 * @param none
+	 * @precondition x = 1
+	 * @postcondition x = 0, 0 < y <= 500
+	 * @return None
+	 */
+	public void UnPark() {
+		// TODO Auto-generated method stub
+		this.x = 0;
+		this.y += 1;
+	}
+	
+	/**
+	 * The method will move the car 5 meters forward from the parking lot, and then do a 
+	 * pre-programmed parallel parking maneuver. 
+	 * 
+	 * @param None
+	 * @precondition x=0, isEmpty() = 0 ?? 
+	 * @postcondition x=1, 
+	 * 
+	 */
+	public void Park() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	/**
+	 * This method will move the car backwards 1 meter, and query the sensors through isEmpty. 
+	 * @param None
+	 * @precondition x = 0, 0 < y <= 500
+	 * @postcondition x = 0, 0 <= y < 500
+	 * @return int [] of current position
+	 * 
+	 */
+	public int[] MoveBackward() {
+		// TODO Auto-generated method stub
+		this.y -= 1;
+		int distance = this.isEmpty();
+		int [] currentSituation = {x, y, distance};
+		
+		return currentSituation;
+		
+	}
+		
 }
+
 
 /*
  Description:
--- src/testing/CarParkingTesting.java
+++ src/testing/CarParkingTesting.java
@@ -2,16 +2,73 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import productioncode.CarParking;
 
 public class CarParkingTesting {
 
 	@Test
-	public void test() {
-		boolean test1 = true;
-		assertTrue(test1);
-	
+	public void testCarParkingWhereIs() {
+		CarParking car = new CarParking(); // Create new car object
+		int [] position = car.WhereIs();
+		
+		assertTrue(position != null); // We need to fail in order to touch the production code
+	}
+	@Test
+	public void testCarParkingIsEmpty() {
+		CarParking car = new CarParking();
+		int distance = car.isEmpty();
+		//assertEquals("Nothing is implemented in isEmpty, so make this fail", 1, distance); // added this line to fail in order to touch the production code
+		assertTrue(distance >= 0);
+		assertTrue(distance <= 200);
+		//System.out.println("Distance to nearest parking-spot " + distance);
+	}
+	@Test
+	public void testCarParkingMoveForward() {
+		CarParking car = new CarParking();
+		int[] pos = car.WhereIs();
+		int [] result = car.MoveForward();
+		assertTrue(pos[0] == result[0]);
+		assertTrue(pos[1] == result[1] - 1);
+		
+		//assertTrue(car.isEmpty() == result[2]); This will fail because of the generation of the sensor-values.
+		
+		//assertTrue(true); // We stopped here, the implementation of the MoveForward methods is missing. 
+	}
+	@Ignore // Due to Park implementation, we can not unPark until we parked.
+	@Test 
+	public void testCarParkingUnPark() { 
+		CarParking car = new CarParking();
+		int [] previousPos = car.WhereIs();
+		car.UnPark();
+		int [] currentPos = car.WhereIs();
+		
+		assertTrue(currentPos[0] == previousPos[0] - 1);
+		assertTrue(currentPos[1] == previousPos[1] + 1);
 		
 	}
+	@Test
+	public void testCarParkingPark() { 
+		CarParking car = new CarParking();
+		car.Park();
+		assertTrue(false); // Stopped here, the implementation for Park is missing.
+	}
+	
+	@Test
+	public void testCarParkingMoveBackward() {
+		CarParking car = new CarParking();
+		car.MoveForward(); car.MoveForward();
+		int [] prevPos = car.WhereIs();
+		int[] currentPos = car.MoveBackward();
+		assertTrue(prevPos[0] == currentPos[0]);
+		assertTrue(prevPos[1] == currentPos[1] + 1);
+		// assertTrue(currentPos[2] == car.isEmpty()); Will fail since isEmpty is random values.
+		
+	}
+	
+	
+	
 
 }
