c0709c1a744b00286951787150d05c1700d418c0
Johannes Hägglund <johagg17@student.hh.se>
2021-09-16 12:29:43 +0200

    Final version of Phase1

--- src/productioncode/CarParking.java
+++ src/productioncode/CarParking.java
@@ -6,19 +6,22 @@ public class CarParking {
 	int x = 0;
 	int y = 0;
 	int freespace = 0;
-	int[] slots = new int[500];
+	int[] slots = new int[501];
 	/**
 	 * Method that will return the current position and the situation of the car, (x, y).
 	 * @param none
 	 * @precondition 0 <= y <= 500, 0 <= x <= 1
 	 * @postcondition 0 <= y <= 500, 0 <= x <= 1
+	 * @Test-cases: 1. MoveForward and confirm the new pos, 2. MoveForward and park, confirm that the car is parked.
 	 * @return int [] of current position
+	 * 
 	 *
 	 */
 	public int [] WhereIs() {
 		// TODO Auto-generated method stub
-		int[] position = {x,y};
+		int[] position = {this.x,this.y, this.freespace};
 		return position;
+		
 	}
 	/**
 	 * Method that will query the two sensors at least 5 times,
@@ -26,42 +29,53 @@ public class CarParking {
 	 * @param None
 	 * @precondition 0 <= y <= 500, x=0
 	 * @postcondition 0 <= y <= 500, x=0,  0 <= sensor values <= 200
+	 * @Test-cases: 1. When a spot is empty, IsEmpty should return true, 2. When a spot is not empty, IsEmpty should return false
+	 * 
 	 * @return int y, distance to nearest parking-spot.
 	 */
-	public boolean isEmpty() {
+	public boolean isEmpty(int fixed_sensor_val) {
 		// TODO Auto-generated method stub
 		int sensor1 = 0;
 		int sensor2 = 0;
 		sensor1 = (int) (Math.random() * 200);
 		sensor2 = (int) (Math.random() * 200);
 		int sensor_val = (sensor1+sensor2)/2;
-		sensor_val = 80;
+		sensor_val = fixed_sensor_val;
 		if(sensor_val > 100){
-			//this.slots[this.y] = 0;
+			this.slots[this.y] = 0;
 			return false;
 		}
 		else {
-			//this.slots[this.y] = 1;
+			this.slots[this.y] = 1;
 			return true;
 		}
 	}
 	/**
 	 *
 	 * @param None
-	 * @return
-	 * @precondition y < 500, x = 0, freespace < 5
+	 * @precondition y < 500, x = 0, freespace < 5 ?? 
 	 * @postcondition y = previous_pos + 1, x = 0, y <= 500
+	 * @Test-cases: 1. MoveForward once and confirm the new position and the situation, 
+	 * 				2. MoveForward 5 times, confirm that 5 empty slots were detected and this.freespace changes to 5. 
+	 * 				3. MoveForward on non-empty slots, confirm the non-empty slots by reading the value from this.freespace.  // We will wait with this test
+	 * 				4. Try to MoveForward when y=500, this should result in not moving. 
+	 * 				5. MoveForward when x=1 (park mode)
 	 * @return a data structure containing the new position and the situation (parked / unparked)
 	 */
 	public int[] MoveForward() {
 		// TODO Auto-generated method stub
+		int [] situation = {this.x, this.y, this.freespace};
+		//System.out.println(this.y);
+		if (this.y == 500 || x == 1) {return situation;} // Added this line to pass the test testCarParkingMoveForwardOutOfBounds. 
 		this.y += 1;
-		boolean noise = this.isEmpty();
+		boolean noise = this.isEmpty(80);
 		if(noise) {
-			this.freespace += 1;
+			if (y >= 5) {
+				this.freespace = slots[y] + slots[y - 1] + slots[y - 2] + slots[y - 3] + slots[y - 4];
+			}
 		}
 		else {
-			this.freespace = 0;
+			// this.freespace = 0;
 		}
 		int [] currentSituation = {x, y, this.freespace};
 
@@ -74,12 +88,15 @@ public class CarParking {
 	 * @param none
 	 * @precondition x = 1
 	 * @postcondition x = 0, 0 < y <= 500
+	 * @Test-cases: 1. Test Unpark when x==0, 2. Test Unpark when x==1, 
 	 * @return None
 	 */
 	public void UnPark() {
 		// TODO Auto-generated method stub
-		this.x = 0;
-		this.y += 1;
+		if (this.x != 0) { // Added this line to pass the testCatParkingUnParkWhenNotParked, precondition
+			this.x = 0;
+			this.MoveForward(); 
+		}
 	}
 
 	/**
@@ -89,6 +106,8 @@ public class CarParking {
 	 * @param None
 	 * @precondition x=0, freespace >= 5
 	 * @postcondition x=1,
+	 * @Test-cases 1. Test Park when unparked but 5 free-slots are not available, 2. Test Park when already parked (x=1), 3. Test Park when there are 5 free-slots
+	 * 			   
 	 *
 	 */
 	public boolean Park() {
@@ -106,20 +125,26 @@ public class CarParking {
 	 * @param None
 	 * @precondition x = 0, 0 < y <= 500
 	 * @postcondition x = 0, 0 <= y < 500
+	 * @Test-cases: 1. Try to MoveBackward when y=0, 2. MoveBackward and confirm that new y-value = prev y - 1. 3. Try to MoveBackward when parked
+	 * 				 
 	 * @return int [] of current position
 	 *
 	 */
 	public int[] MoveBackward() {
 		// TODO Auto-generated method stub
+		int [] situation = {this.x, this.y, this.freespace};
+		if (this.y == 0 || x == 1) {return situation;} // Added this line to pass the test testCarParkingMoveBackWardAtStart. 
 		this.y -= 1;
-		boolean noise = this.isEmpty();
+		boolean noise = this.isEmpty(80);
 		if(noise) {
-			this.freespace += 1;
+			if (this.y >= 5) {
+				this.freespace = slots[this.y] + slots[this.y - 1] + slots[this.y - 2] + slots[this.y - 3] + slots[this.y - 4];
+			}
 		}
 		else {
-			this.freespace = 0;
+			// this.freespace = 0;
 		}
-		int [] currentSituation = {x, y, this.freespace};
+		int [] currentSituation = {this.x, this.y, this.freespace};
 
 		return currentSituation;
 
